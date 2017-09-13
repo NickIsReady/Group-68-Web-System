b2a96deca2f36c9a7580835af8cb72aa21bc65d4
Doug Brennan <Doug Brennan@172.19.38.62>
2017-09-13 11:05:53 +1000

    informtion page starters

--- ifb299website/settings.py
+++ ifb299website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'informationPage.apps.InformationpageConfig',
     'websiteMain.apps.WebsitemainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- ifb299website/urls.py
+++ ifb299website/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^websiteMain/', include('websiteMain.urls')),
+    url(r'^informationPage/', include('informationPage.urls')),
 ]
